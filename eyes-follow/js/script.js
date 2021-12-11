f812ded61655672011f5553e3bed94a30fea6259
丘惟夫 <waveciou@gmail.com>
2021-12-11 19:32:03 +0800

    sort up project

--- eyes-follow/js/script.js
+++ eyes-follow/js/script.js
@@ -1,28 +1,28 @@
 (function () {
-    const $overlay = $('.overlay');
-    const overlayWidth = $overlay.innerWidth();
-    const overlayHeight = $overlay.innerHeight();
-    const $ball = $('.ball');
-    let percentX = 0;
-    let percentY = 0;
-    let mouseX, mouseY;
+  const $overlay = $('.overlay');
+  const overlayWidth = $overlay.innerWidth();
+  const overlayHeight = $overlay.innerHeight();
+  const $ball = $('.ball');
+  let percentX = 0;
+  let percentY = 0;
+  let mouseX, mouseY;
 
-    function setPosition(positionX, positionY) {
-        $ball.css({
-            '-webkit-transform': `translate(${positionX}%, ${positionY}%)`,
-            'transform': `translate(${positionX}%, ${positionY}%)`
-        });
-    }
-
-    $overlay.on('mousemove', function (e) {
-        mouseX = e.offsetX;
-        mouseY = e.offsetY;
-
-        percentX = (Math.floor((mouseX / overlayWidth) * 100) - 50) * 2;
-        percentY = (Math.floor((mouseY / overlayHeight) * 100) - 50) * 2;
-
-        console.log(percentX, percentY)
-
-        setPosition(percentX, percentY);
+  function setPosition(positionX, positionY) {
+    $ball.css({
+      '-webkit-transform': `translate(${positionX}%, ${positionY}%)`,
+      'transform': `translate(${positionX}%, ${positionY}%)`
     });
+  }
+
+  $overlay.on('mousemove', function (e) {
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+
+    percentX = (Math.floor((mouseX / overlayWidth) * 100) - 50) * 2;
+    percentY = (Math.floor((mouseY / overlayHeight) * 100) - 50) * 2;
+
+    console.log(percentX, percentY)
+
+    setPosition(percentX, percentY);
+  });
 })()
